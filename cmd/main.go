c0173cb115e60f78c9f7ca52572091eba106777e
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2026-02-02 20:38:13 +0900

    require ctx for logging and use structured fields

--- cmd/main.go
+++ cmd/main.go
@@ -3,18 +3,17 @@ package main
 import (
 	"context"
 
-	api "github.com/daisuke-harada/date-courses-go/internal/infrastructure/cmd/api"
+	"github.com/daisuke-harada/date-courses-go/internal/infrastructure/cmd/api"
 	"github.com/daisuke-harada/date-courses-go/pkg/logger"
 )
 
 func main() {
-	// ロガーを初期化
-	if err := logger.NewLogger(); err != nil {
+	if err := logger.Init(); err != nil {
 		panic(err)
 	}
-	defer logger.CloseLogger()
+	defer logger.Close()
 
 	if err := api.Run(context.Background()); err != nil {
-		logger.Error("Failed to start server :%v", err)
+		logger.Error(context.Background(), "Failed to start server", "err", err)
 	}
 }
