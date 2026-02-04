e57d339ed39ed336fe056a4799ac5ce546cdbf5a
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2026-02-04 20:04:25 +0900

    make gen

--- internal/infrastructure/cmd/api/handler/handler.go
+++ internal/infrastructure/cmd/api/handler/handler.go
@@ -1,36 +1,37 @@
 package handler
 
 type Handler struct {
-	DeleteApiV1CoursesIdHandler
-	DeleteApiV1DateSpotReviewsIdHandler
-	DeleteApiV1DateSpotsIdHandler
-	DeleteApiV1RelationshipsCurrentUserIdOtherUserIdHandler
-	DeleteApiV1UsersIdHandler
-	GetHandler
-	GetApiV1CoursesHandler
-	GetApiV1CoursesIdHandler
-	GetApiV1DateSpotsHandler
-	GetApiV1DateSpotsIdHandler
-	GetApiV1GenresIdHandler
-	GetApiV1PrefecturesIdHandler
-	GetApiV1TopHandler
-	GetApiV1UsersHandler
-	GetApiV1UsersIdHandler
-	GetApiV1UsersUserIdFollowersHandler
-	GetApiV1UsersUserIdFollowingsHandler
-	PostApiV1CoursesHandler
-	PostApiV1CoursesSortHandler
-	PostApiV1DateSpotNameSearchHandler
-	PostApiV1DateSpotReviewsHandler
-	PostApiV1DateSpotsHandler
-	PostApiV1DateSpotsSortHandler
-	PostApiV1LoginHandler
-	PostApiV1RelationshipsHandler
-	PostApiV1SignupHandler
-	PostApiV1UserNameSearchHandler
-	PutApiV1DateSpotReviewsIdHandler
-	PutApiV1DateSpotsIdHandler
-	PutApiV1UsersIdHandler
+  DeleteApiV1CoursesIdHandler
+  DeleteApiV1DateSpotReviewsIdHandler
+  DeleteApiV1DateSpotsIdHandler
+  DeleteApiV1RelationshipsCurrentUserIdOtherUserIdHandler
+  DeleteApiV1UsersIdHandler
+  GetHandler
+  GetApiV1CoursesHandler
+  GetApiV1CoursesIdHandler
+  GetApiV1DateSpotsHandler
+  GetApiV1DateSpotsIdHandler
+  GetApiV1GenresIdHandler
+  GetApiV1PrefecturesIdHandler
+  GetApiV1TopHandler
+  GetApiV1UsersHandler
+  GetApiV1UsersIdHandler
+  GetApiV1UsersUserIdFollowersHandler
+  GetApiV1UsersUserIdFollowingsHandler
+  PostApiV1CoursesHandler
+  PostApiV1CoursesSortHandler
+  PostApiV1DateSpotNameSearchHandler
+  PostApiV1DateSpotReviewsHandler
+  PostApiV1DateSpotsHandler
+  PostApiV1DateSpotsSortHandler
+  PostApiV1LoginHandler
+  PostApiV1RelationshipsHandler
+  PostApiV1SignupHandler
+  PostApiV1UserNameSearchHandler
+  PutApiV1DateSpotReviewsIdHandler
+  PutApiV1DateSpotsIdHandler
+  PutApiV1UsersIdHandler
+  
 }
 
 func NewHandler() *Handler {
