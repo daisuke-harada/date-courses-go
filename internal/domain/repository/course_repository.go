419b2474a08b73a72c08645244cc34714cf26c6c
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2026-04-18 08:58:28 +0900

    feat: add FindByID to CourseRepository interface #59

--- internal/domain/repository/course_repository.go
+++ internal/domain/repository/course_repository.go
@@ -10,4 +10,5 @@ type CourseRepository interface {
 	Create(ctx context.Context, course *model.Course) error
 	FindByUserID(ctx context.Context, userID uint) ([]*model.Course, error)
 	FindAll(ctx context.Context) ([]*model.Course, error)
+	FindByID(ctx context.Context, id uint) (*model.Course, error)
 }
