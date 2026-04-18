07dd45965c2d27c4bcc53edacb2d71938ceae958
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2026-04-18 09:02:07 +0900

    feat: implement GET /api/v1/courses/:id handler #59

--- internal/interface/handler/get_api_v1_courses_id.go
+++ internal/interface/handler/get_api_v1_courses_id.go
@@ -2,13 +2,27 @@ package handler
 
 import (
 	"net/http"
+
+	"github.com/daisuke-harada/date-courses-go/internal/usecase"
 	"github.com/labstack/echo/v4"
 )
 
-type GetApiV1CoursesIdHandler struct {}
+type GetApiV1CoursesIdHandler struct {
+	InputPort usecase.GetCourseInputPort
+}
 
-func (h *GetApiV1CoursesIdHandler) GetApiV1CoursesId(ctx echo.Context, arg1 int ) error {
-	// TODO: Implement your logic here
-	// Example: return ctx.JSON(http.StatusOK, map[string]string{"message": "success"})
-	return ctx.JSON(http.StatusOK, map[string]string{"message": "success"})
+func (h *GetApiV1CoursesIdHandler) GetApiV1CoursesId(ctx echo.Context, arg1 int) error {
+	output, err := h.InputPort.Execute(ctx.Request().Context(), usecase.GetCourseInput{CourseID: uint(arg1)})
+	if err != nil {
+		return err
+	}
+	course := output.Course
+	return ctx.JSON(http.StatusOK, map[string]interface{}{
+		"id":          course.ID,
+		"user_id":     course.UserID,
+		"travel_mode": course.TravelMode,
+		"authority":   course.Authority,
+		"created_at":  course.CreatedAt.Format("2006-01-02T15:04:05Z07:00"),
+		"updated_at":  course.UpdatedAt.Format("2006-01-02T15:04:05Z07:00"),
+	})
 }
