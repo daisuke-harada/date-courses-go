7a7628f8944efa442e281baee0b2fbc01e6850e5
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2026-04-18 10:19:20 +0900

    feat: implement DELETE /api/v1/date_spot_reviews/:id handler (Green) #54

--- internal/interface/handler/delete_api_v1_date_spot_reviews_id.go
+++ internal/interface/handler/delete_api_v1_date_spot_reviews_id.go
@@ -2,13 +2,21 @@ package handler
 
 import (
 	"net/http"
+
+	"github.com/daisuke-harada/date-courses-go/internal/usecase"
 	"github.com/labstack/echo/v4"
 )
 
-type DeleteApiV1DateSpotReviewsIdHandler struct {}
+type DeleteApiV1DateSpotReviewsIdHandler struct {
+	InputPort usecase.DeleteDateSpotReviewInputPort
+}
 
-func (h *DeleteApiV1DateSpotReviewsIdHandler) DeleteApiV1DateSpotReviewsId(ctx echo.Context, arg1 int ) error {
-	// TODO: Implement your logic here
-	// Example: return ctx.JSON(http.StatusOK, map[string]string{"message": "success"})
+func (h *DeleteApiV1DateSpotReviewsIdHandler) DeleteApiV1DateSpotReviewsId(ctx echo.Context, arg1 int) error {
+	_, err := h.InputPort.Execute(ctx.Request().Context(), usecase.DeleteDateSpotReviewInput{
+		ReviewID: uint(arg1),
+	})
+	if err != nil {
+		return err
+	}
 	return ctx.JSON(http.StatusOK, map[string]string{"message": "success"})
 }
